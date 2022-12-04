182b0383ed8851d5ddf8c6d51f8bace1f6a00209
Abrosss <nataroro5@gmail.com>
2022-12-05 00:19:42 +0600

    fixed font size of request code on api page

--- src/pages/ApiInfo.js
+++ src/pages/ApiInfo.js
@@ -24,9 +24,9 @@ function ApiInfo() {
       <section className='apiInfo'>
         <h1>Get verb information</h1>
         <p><span className='bold'>Usage:</span> to get meaning of a Spanish verb, you can send request to</p>
-        <p><code><span className='bold'>https://spanish-verbs-api.vercel.app/api/verbs/</span><span className='italic colored'>{tag}</span></code></p>
+        <p className='request'><code><span className='bold'>https://spanish-verbs-api.vercel.app/api/verbs/</span><span className='italic colored'>{tag}</span></code></p>
         <p className='bold'>Example:</p>
-        <p className='exampleRequest'><code><span className='bold'>https://spanish-verbs-api.vercel.app/api/verbs/</span><span className='italic'>pinto</span></code></p>
+        <p className='request exampleRequest'><code><span className='bold'>https://spanish-verbs-api.vercel.app/api/verbs/</span><span className='italic'>pinto</span></code></p>
         <pre>
           <code>{snippet}</code>
         </pre>
